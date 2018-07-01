1f7aacac0c71192820130a9b2c94b30880d7e0bd
joeusza <37946812+joeusza@users.noreply.github.com>
2018-06-30 18:24:50 -0700

    Add Flexbox and Media Queries

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: 'pk.eyJ1Ijoiam9ldXN6YSIsImEiOiJjamlhd2Q3b3AxYm4wM3FxcXprNjEzZG5pIn0.FdVyxxdiv1rNqTVcumfg_Q',
+    mapboxToken: '<your MAPBOX API KEY HERE>',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
