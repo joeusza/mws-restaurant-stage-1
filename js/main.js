8daedfd454b37cf7145c61f282db9274f2ecee07
joeusza <37946812+joeusza@users.noreply.github.com>
2018-10-23 20:46:45 -0700

    Move idb to Service Worker, Exclude Restaurants Json From Service Worker

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
-  // suggested by https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
-  .then(function(reg) {
-    // registration worked
-    console.log(`Registration succeeded. Scope is ${reg.scope}`);
-  }).catch(function(error) {
-    // registration failed
-    console.log(`Registration failed with ${error}`);
-  });
-}
-
 let restaurants,
   neighborhoods,
   cuisines
--- sw.js
+++ sw.js
@@ -16,14 +16,9 @@ const mainAssets = [
 
 function createRestDB() {
   idb.open('rest-db', 1, upgradeDb => {
-    switch(upgradeDb.oldVersion) {
-      case 0:
-      upgradeDb.createObjectStore('restaurants', {keypath: 'id'});
-    }
+    upgradeDb.createObjectStore('restaurants', {keypath: 'id'});
   });
 }
-
-
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
@@ -39,100 +34,11 @@ self.addEventListener('activate', function(event) {
     createRestDB()
     );
   });
-//   event.waitUntil(
-//     caches.keys().then(function(cacheNames) {
-//       return Promise.all(
-//         cacheNames.filter(function(cacheName) {
-//           return cacheName.startsWith('mws-restaurant') &&
-//                  cacheName != staticCacheName;
-//         }).map(function(cacheName) {
-//           return caches.delete(cacheName);
-//         })
-//       );
-//     })
-//   );
-// });
-//
-//
-// // //  If request is not already in cache,
+
+// //  If request is not already in cache,
 self.addEventListener('fetch', function(event) {
-  // console.log(event.request);
-  // Prevent caching to browser cache, on advice of Oksana K. MWS
-// const checkURL = new URL(event.request.url);
-if (event.request.url.includes('restaurants')) {
-  console.log(event.request.url);
-
-  event.respondWith(
-    fetch(event.request)
-    .then(fetchResponse => fetchResponse.json())
-    .then(function(resJson){
-      console.log(resJson);
-      return resJson;
-    })
-    .then(function(finalResponse) {
-      // let finalClone = finalResponse.clone();
-      // console.log(finalClone);
-      return new Response(JSON.stringify(finalResponse));
-    }));
-    // .then(finalResponse => {
-    //   return new Response(JSON.stringify(finalResponse));
-    // }));
-
-
-  // idb.open('rest-db', 1)
-  // .then(db => {return db.transaction('restaurants', 'readonly')
-  // .objectStore('restaurants').getAll();
-  // })
-  // .then(function(allRestaurants) {
-  // let restLength = allRestaurants.length;
-  // if (restLength > 0) {
-  //   console.log('some');
-  //   return allRestaurants;
-  // } else {
-  //   console.log('none');
-  //   fetch(event.request.url)
-  //   .then(function(response) {
-  //     idb.open('rest-db', 1)
-  //     .then(db => {return db.transaction('restaurants', 'readonly')
-  //     return response.json();
-  //   });
-  // }
-  // }));
-
-
-  // if (restLength > 0) {
-  //   console.log('some')
-  //   return allRestaurants;
-  // } else {
-  //   console.log('none')
-  //   fetch(event.request.url)
-  //   .then(function(response) {
-  //           return response.json();
-
-  }
-
-
-
-
-
-  //   .then(function(restObjs) {
-  //     function addRest2Db(restObj) {
-  //         // console.log(restObj.id);
-  //         idb.open('rest-db', 1)
-  //         .then(db => {
-  //         const tx = db.transaction('restaurants', 'readwrite');
-  //         tx.objectStore('restaurants').put(restObj, restObj.id);
-  //         return tx.complete;
-  //       });
-  //     }
-  //     for (const restObj of restObjs) {
-  //       addRest2Db(restObj);
-  //       }
-  //       console.log('got it!');
-  //     });
-  // });
-  // );
-  else {
+if (!event.request.url.includes('restaurants')) {
+  // console.log('sw caching', event.request.url);
   let cacheRequest = event.request;
   // Check if `restaurant.html` is anywhere in the requested URL. If it is, respond with the `restaurant.html` page.
   let restPage = 'restaurant.html';
