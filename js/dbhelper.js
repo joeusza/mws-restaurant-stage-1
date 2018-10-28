548682323405b5bd113854dccfa85d675c6f1704
joeusza <37946812+joeusza@users.noreply.github.com>
2018-10-28 12:02:07 -0700

    Fix Broken Manifest

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -1,6 +1,5 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js')
-      // suggested by https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
     .then(function(reg) {
     // registration worked
       console.log(`Registration succeeded. Scope is ${reg.scope}`);
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const staticCacheName = 'mws-restaurant-v1';
+const staticCacheName = 'mws-restaurant-v6';
 const mainAssets = [
   '/',
   '/index.html',
@@ -9,13 +9,38 @@ const mainAssets = [
   '/js/main.js',
   '/js/dbhelper.js',
   '/js/restaurant_info.js',
-  '/js/idb.js'
+  '/js/idb.js',
+  '/manifest.webmanifest',
+  '/img/icons/icon-72x72.png',
+  '/img/icons/icon-96x96.png',
+  '/img/icons/icon-128x128.png',
+  '/img/icons/icon-144x144.png',
+  '/img/icons/icon-152x152.png',
+  '/img/icons/icon-192x192.png',
+  '/img/icons/icon-384x384.png',
+  '/img/icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(staticCacheName).then(function(cache) {
       return cache.addAll(mainAssets);
+    })
+  );
+});
+
+// Delete old cache(s) so that only latest will be available
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('mws-restaurant') &&
+                 cacheName != staticCacheName;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
     })
   );
 });
